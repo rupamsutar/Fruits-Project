d6b3ce527610d9bf3a87d6399508511bfd2e0686
Rupam Sutar <rupamsutar281998@gmail.com>
2022-09-02 20:50:33 +0530

    Data validation

--- app.js
+++ app.js
@@ -7,36 +7,44 @@ mongoose.connect("mongodb://localhost:27017/fruitsDB")
 
 //insert objects
 const fruitSchema = new mongoose.Schema({
-  name: String,
-  rating: Number,
+  name: {
+    type: String,
+    required: [true, "Please enter the name !"]
+  },
+  rating:{
+    type: Number,
+    min: 1,
+    max: 10
+  },
   review: String
 });
 
 const Fruit = mongoose.model("Fruit", fruitSchema);
 
 const fruit = new Fruit ({
-  name: "Apple",
-  rating: 8,
-  review:"An apple a day keeps doctor away"
+  rating:4,
+  review:"Sour"
 });
 
-const kiwi = new Fruit ({
-  name:"Kiwi",
-  rating: 6,
-  review:"Too Costly"
-});
+fruit.save();
 
-const banana = new Fruit( {
-  name: "banana",
-  rating: 9,
-  review: "Rich in zinc"
-});
-
-const pomegranate = new Fruit({
-  name: "pomegranate",
-  rating: 7,
-  review: "Swettens the cum"
-});
+// const kiwi = new Fruit ({
+//   name:"Kiwi",
+//   rating: 6,
+//   review:"Too Costly"
+// });
+//
+// const banana = new Fruit( {
+//   name: "banana",
+//   rating: 9,
+//   review: "Rich in zinc"
+// });
+//
+// const pomegranate = new Fruit({
+//   name: "pomegranate",
+//   rating: 7,
+//   review: "Swettens the cum"
+// });
 
 // Fruit.insertMany([kiwi,banana,pomegranate], function(err) {
 //   if(err){
@@ -53,12 +61,12 @@ const personSchema = new mongoose.Schema({
 
 const Member = mongoose.model("Member", personSchema);
 
-const member = new Member({
-  name: "Angela",
-  age: 37
-});
-
-member.save();
+// const member = new Member({
+//   name: "Angela",
+//   age: 37
+// });
+//
+// member.save();
 
 
 Fruit.find(function(err, fruits) {
@@ -66,7 +74,7 @@ Fruit.find(function(err, fruits) {
     console.log(err);
   } else {
     mongoose.connection.close();
-    
+
     for (var i = 0; i < fruits.length; i++) {
       console.log(fruits[i].name)
     };
