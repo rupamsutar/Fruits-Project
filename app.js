ac0114ef7479cd3be1547fb5bc46f0649bda006d
Rupam Sutar <rupamsutar281998@gmail.com>
2022-09-03 11:07:44 +0530

    learnt to add the realtionships between the data

--- app.js
+++ app.js
@@ -17,13 +17,14 @@ const fruitSchema = new mongoose.Schema({
 });
 
  const Fruit = mongoose.model("Fruit", fruitSchema);
-//
-// const fruit = new Fruit ({
-//   rating:4,
-//   review:"Sour"
-// });
-//
-// fruit.save();
+
+const coconut = new Fruit ({
+  name: "coconut",
+  rating:4,
+  review:"Sour"
+});
+
+// coconut.save();
 
 // const kiwi = new Fruit ({
 //   name:"Kiwi",
@@ -31,7 +32,7 @@ const fruitSchema = new mongoose.Schema({
 //   review:"Too Costly"
 // });
 //
-// const banana = new Fruit( {
+// const banansa = new Fruit( {
 //   name: "banana",
 //   rating: 9,
 //   review: "Rich in zinc"
@@ -53,45 +54,56 @@ const fruitSchema = new mongoose.Schema({
 
 const personSchema = new mongoose.Schema({
   name: String,
-  age: Number
+  age: Number,
+  favouriteFruit: fruitSchema
 });
 
 const Member = mongoose.model("Member", personSchema);
 
-// const member = new Member({
-//   name: "Angela",
-//   age: 37
+const member = new Member({
+  name: "Chiku",
+  age: 24,
+  favouriteFruit: coconut
+});
+
+member.save();
+
+
+// Fruit.find(function(err, fruits) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     mongoose.connection.close();
+//
+//     for (var i = 0; i < fruits.length; i++) {
+//       console.log(fruits[i].name)
+//     };
+//   };
+// });
+
+
+
+// Fruit.updateOne({_id: "631227b189a502696b3d59ad"}, {name: "Peach"}, function(err) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log("successfully updated the document");
+//   };
 // });
 //
-// member.save();
+// Fruit.deleteOne({name: "Peach"}, function(err) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successfully deleted the document");
+//   };
+// });
 
 
-Fruit.find(function(err, fruits) {
-  if (err) {
-    console.log(err);
-  } else {
-    mongoose.connection.close();
-
-    for (var i = 0; i < fruits.length; i++) {
-      console.log(fruits[i].name)
-    };
-  };
-});
-
-
-
-Fruit.updateOne({_id: "631227b189a502696b3d59ad"}, {name: "Peach"}, function(err) {
-  if(err) {
-    console.log(err);
-  } else {
-    console.log("successfully updated the document");
-  };
-});
-
-Fruit.deleteOne({name: "Peach"}, function(err) {
-  if(err) {
-    console.log(err);
-  } else {
-    console.log("Successfully deleted the document");
-  };
-});
+// Member.deleteMany({name: "Angela"}, function(err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successfully deleted all the entries");
+//   };
+// });
